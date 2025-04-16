f083720d37de0d7374424081c17924b54fc11c5d
alvinouille <enjoy011113@gmail.com>
2025-04-16 18:27:08 +0200

    remove config endpoint

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,10 +1,8 @@
 import express, { Router } from 'express';
-import configRouter from './config';
 import cairocoderRouter from './cairocoder';
 
 const router: Router = express.Router();
 
-router.use('/config', configRouter);
 router.use('/generate', cairocoderRouter);
 
 export default router;
