a805a1007555efe9f7c72337159a02cdffc4d959
alvinouille <alvinalesaint@protonmail.com>
2025-05-06 19:17:37 +0200

    fix: remove unnecessary logs from script test

--- scripts/snak-test.sh
+++ scripts/snak-test.sh
@@ -6,8 +6,6 @@ TEST_RESULT=${PIPESTATUS[0]}
 
 if [ $TEST_RESULT -ne 0 ]; then
     echo "❌ API test failed with exit code $TEST_RESULT!"
-    echo "=============== Test Logs ==============="
-    grep -A 5 -E "TEST FAILED|❌" test-output.log || true
     exit 1
 else
     echo "✅ API test passed!"
