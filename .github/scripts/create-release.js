52f193d42f6a096fab0ee5fe31af29c8a0808b21
Alvina <alvinalesaint@protonmail.com>
2025-06-18 15:40:38 +0200

    ci: add release workflow and update dependances (#17)

---
+++ .github/scripts/create-release.js
@@ -0,0 +1,30 @@
+module.exports = async ({ github, context }) => {
+  const newTag = process.env.NEW_TAG;
+  const repository = process.env.REPOSITORY;
+
+  const { data } = await github.rest.repos.generateReleaseNotes({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    tag_name: newTag,
+  });
+
+  const dockerInfo = `
+
+---
+  As with all our previous releases, you can find the Docker images:
+- [Backend](https://ghcr.io/${repository}/backend:${newTag})
+- [Ingester](https://ghcr.io/${repository}/ingester:${newTag})
+
+
+${data.body}`;
+
+  await github.rest.repos.createRelease({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    tag_name: newTag,
+    name: data.name,
+    body: dockerInfo,
+    draft: true,
+    prerelease: false,
+  });
+};
