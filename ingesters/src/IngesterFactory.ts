fcb195bd88f65a65412a819a3d75cc8c9c6ca854
Etienne Duclos <ledude2707@gmail.com>
2025-12-01 12:48:13 +0100

    p7utting back ingester factory

--- ingesters/src/IngesterFactory.ts
+++ ingesters/src/IngesterFactory.ts
@@ -49,6 +49,9 @@ export class IngesterFactory {
       case 'scarb_docs':
         return new ScarbDocsIngester();
 
+      case 'starknet_js':
+        return new StarknetJSIngester();
+
       case 'starknet_blog':
         return new StarknetBlogIngester();
 
